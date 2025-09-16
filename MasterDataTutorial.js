d270542dc68c15783f308c3785c3e159a99f0adf
MercuryVoice <tsengmercury@gmail.com>
2025-09-16 17:38:09 +0800

    Initial commit: Update MasterDataAnalyer project files

--- MasterDataTutorial.js
+++ MasterDataTutorial.js
@@ -10,6 +10,26 @@
 // =================================================================================================
 
 /**
+ * MasterDataAnalyzer - A Google Sheets Add-on for intelligent data operations.
+ *
+ * Copyright (c) 2025 Tata Sum (mda.design)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * A complete dictionary of all strings for the tutorial sidebar with CORRECT sheet names.
  * @returns {Object.<string, string>} An object containing all UI strings in the correct language.
  */
